e37de48f8f1fc4ee44ab261002331cf35a8ddb07
chinthu111 <41237688+chinthu111@users.noreply.github.com>
2025-06-03 15:46:53 +0200

    Update gql.py

--- gql.py
+++ gql.py
@@ -1,26 +1,12 @@
-from pycore import IsdOperationParams
+from common import ct
 
-class WriteUpdateAssetPosition:
-    def __init__(self, asset_name, latitude, longitude, altitude):
-        self.asset_name = asset_name
-        self.latitude = latitude
-        self.longitude = longitude
-        self.altitude = altitude
-
-    def to_gql(self):
-        mutation = """
-        mutation UpdateAssetPosition($asset_name: String!, $lat: Float!, $lon: Float!, $alt: Float!) {
-            updateAssetPosition(assetName: $asset_name, latitude: $lat, longitude: $lon, altitude: $alt) {
-                success
-                message
-            }
+def WriteUpdateAssetPosition(asset_name, latitude, longitude, altitude):
+    return {
+        "op": "UpdateAssetPosition",
+        "params": {
+            "asset_name": asset_name,
+            "latitude": latitude,
+            "longitude": longitude,
+            "altitude": altitude
         }
-        """
-        # Proper creation of IsdOperationParams
-        op_params = IsdOperationParams()
-        op_params.add_param("asset_name", self.asset_name)
-        op_params.add_param("lat", self.latitude)
-        op_params.add_param("lon", self.longitude)
-        op_params.add_param("alt", self.altitude)
-
-        return mutation, op_params
+    }
